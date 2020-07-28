801ac222a2e5a8a8543258c97531befc91f18fc4
Bradyn Brown <17073@bayfield-high.school.nz>
2020-07-29 11:24:00 +1200

    add initial files

---
+++ CanteenMenuAssessment/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace CanteenMenuAssessment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.picBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnLunch = new System.Windows.Forms.Button();
+            this.lblCheeseRolls = new System.Windows.Forms.Label();
+            this.lblMuffinSavoury = new System.Windows.Forms.Label();
+            this.lblSavouryPinwheel = new System.Windows.Forms.Label();
+            this.lblCinnamonSwirl = new System.Windows.Forms.Label();
+            this.lblMuffinSweet = new System.Windows.Forms.Label();
+            this.lblBrownie = new System.Windows.Forms.Label();
+            this.lblCheesePuffs = new System.Windows.Forms.Label();
+            this.lblSandwichOptions = new System.Windows.Forms.Label();
+            this.lblSandwichs = new System.Windows.Forms.Label();
+            this.lblBagelOptions = new System.Windows.Forms.Label();
+            this.lblBagels = new System.Windows.Forms.Label();
+            this.lblSandwichOptions2 = new System.Windows.Forms.Label();
+            this.lblSandwichOptions3 = new System.Windows.Forms.Label();
+            this.lblSandwichOptions4 = new System.Windows.Forms.Label();
+            this.lblSandwichOptions5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 20F);
+            this.label1.ForeColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(23, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(423, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bayfield Highschool Canteen Menu";
+            // 
+            // picBox1
+            // 
+            this.picBox1.Image = global::CanteenMenuAssessment.Properties.Resources.BayfieldSheild;
+            this.picBox1.Location = new System.Drawing.Point(205, 1);
+            this.picBox1.Name = "picBox1";
+            this.picBox1.Size = new System.Drawing.Size(64, 82);
+            this.picBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox1.TabIndex = 1;
+            this.picBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(136, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 24);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Interval";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnLunch
+            // 
+            this.btnLunch.Location = new System.Drawing.Point(241, 137);
+            this.btnLunch.Name = "btnLunch";
+            this.btnLunch.Size = new System.Drawing.Size(99, 24);
+            this.btnLunch.TabIndex = 3;
+            this.btnLunch.Text = "Lunch";
+            this.btnLunch.UseVisualStyleBackColor = true;
+            // 
+            // lblCheeseRolls
+            // 
+            this.lblCheeseRolls.AutoSize = true;
+            this.lblCheeseRolls.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblCheeseRolls.Location = new System.Drawing.Point(26, 169);
+            this.lblCheeseRolls.Name = "lblCheeseRolls";
+            this.lblCheeseRolls.Size = new System.Drawing.Size(91, 17);
+            this.lblCheeseRolls.TabIndex = 4;
+            this.lblCheeseRolls.Text = "Cheese Rolls";
+            // 
+            // lblMuffinSavoury
+            // 
+            this.lblMuffinSavoury.AutoSize = true;
+            this.lblMuffinSavoury.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblMuffinSavoury.Location = new System.Drawing.Point(26, 203);
+            this.lblMuffinSavoury.Name = "lblMuffinSavoury";
+            this.lblMuffinSavoury.Size = new System.Drawing.Size(109, 17);
+            this.lblMuffinSavoury.TabIndex = 5;
+            this.lblMuffinSavoury.Text = "Muffin - savoury";
+            // 
+            // lblSavouryPinwheel
+            // 
+            this.lblSavouryPinwheel.AutoSize = true;
+            this.lblSavouryPinwheel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblSavouryPinwheel.Location = new System.Drawing.Point(26, 186);
+            this.lblSavouryPinwheel.Name = "lblSavouryPinwheel";
+            this.lblSavouryPinwheel.Size = new System.Drawing.Size(120, 17);
+            this.lblSavouryPinwheel.TabIndex = 6;
+            this.lblSavouryPinwheel.Text = "Savoury Pinwheel";
+            // 
+            // lblCinnamonSwirl
+            // 
+            this.lblCinnamonSwirl.AutoSize = true;
+            this.lblCinnamonSwirl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblCinnamonSwirl.Location = new System.Drawing.Point(26, 237);
+            this.lblCinnamonSwirl.Name = "lblCinnamonSwirl";
+            this.lblCinnamonSwirl.Size = new System.Drawing.Size(104, 17);
+            this.lblCinnamonSwirl.TabIndex = 7;
+            this.lblCinnamonSwirl.Text = "Cinnamon Swirl";
+            // 
+            // lblMuffinSweet
+            // 
+            this.lblMuffinSweet.AutoSize = true;
+            this.lblMuffinSweet.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblMuffinSweet.Location = new System.Drawing.Point(26, 220);
+            this.lblMuffinSweet.Name = "lblMuffinSweet";
+            this.lblMuffinSweet.Size = new System.Drawing.Size(95, 17);
+            this.lblMuffinSweet.TabIndex = 8;
+            this.lblMuffinSweet.Text = "Muffin - sweet";
+            // 
+            // lblBrownie
+            // 
+            this.lblBrownie.AutoSize = true;
+            this.lblBrownie.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblBrownie.Location = new System.Drawing.Point(26, 254);
+            this.lblBrownie.Name = "lblBrownie";
+            this.lblBrownie.Size = new System.Drawing.Size(58, 17);
+            this.lblBrownie.TabIndex = 9;
+            this.lblBrownie.Text = "Brownie";
+            // 
+            // lblCheesePuffs
+            // 
+            this.lblCheesePuffs.AutoSize = true;
+            this.lblCheesePuffs.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblCheesePuffs.Location = new System.Drawing.Point(26, 271);
+            this.lblCheesePuffs.Name = "lblCheesePuffs";
+            this.lblCheesePuffs.Size = new System.Drawing.Size(92, 17);
+            this.lblCheesePuffs.TabIndex = 10;
+            this.lblCheesePuffs.Text = "Cheese Puffs";
+            this.lblCheesePuffs.Click += new System.EventHandler(this.lblCheesePuffs_Click);
+            // 
+            // lblSandwichOptions
+            // 
+            this.lblSandwichOptions.AutoSize = true;
+            this.lblSandwichOptions.Location = new System.Drawing.Point(35, 305);
+            this.lblSandwichOptions.Name = "lblSandwichOptions";
+            this.lblSandwichOptions.Size = new System.Drawing.Size(157, 13);
+            this.lblSandwichOptions.TabIndex = 11;
+            this.lblSandwichOptions.Text = "beef, cheese, lettuce and onion";
+            // 
+            // lblSandwichs
+            // 
+            this.lblSandwichs.AutoSize = true;
+            this.lblSandwichs.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblSandwichs.Location = new System.Drawing.Point(26, 288);
+            this.lblSandwichs.Name = "lblSandwichs";
+            this.lblSandwichs.Size = new System.Drawing.Size(83, 17);
+            this.lblSandwichs.TabIndex = 12;
+            this.lblSandwichs.Text = "Sandwiches";
+            // 
+            // lblBagelOptions
+            // 
+            this.lblBagelOptions.AutoSize = true;
+            this.lblBagelOptions.Location = new System.Drawing.Point(35, 387);
+            this.lblBagelOptions.Name = "lblBagelOptions";
+            this.lblBagelOptions.Size = new System.Drawing.Size(301, 13);
+            this.lblBagelOptions.TabIndex = 13;
+            this.lblBagelOptions.Text = "smoked salmon, creamy cheese, alfa sprouts and tomato relish";
+            // 
+            // lblBagels
+            // 
+            this.lblBagels.AutoSize = true;
+            this.lblBagels.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblBagels.Location = new System.Drawing.Point(26, 370);
+            this.lblBagels.Name = "lblBagels";
+            this.lblBagels.Size = new System.Drawing.Size(51, 17);
+            this.lblBagels.TabIndex = 14;
+            this.lblBagels.Text = "Bagels";
+            this.lblBagels.Click += new System.EventHandler(this.lblBagels_Click);
+            // 
+            // lblSandwichOptions2
+            // 
+            this.lblSandwichOptions2.AutoSize = true;
+            this.lblSandwichOptions2.Location = new System.Drawing.Point(35, 318);
+            this.lblSandwichOptions2.Name = "lblSandwichOptions2";
+            this.lblSandwichOptions2.Size = new System.Drawing.Size(176, 13);
+            this.lblSandwichOptions2.TabIndex = 15;
+            this.lblSandwichOptions2.Text = "ham, tomato, egg, lettuce and mayo";
+            // 
+            // lblSandwichOptions3
+            // 
+            this.lblSandwichOptions3.AutoSize = true;
+            this.lblSandwichOptions3.Location = new System.Drawing.Point(35, 331);
+            this.lblSandwichOptions3.Name = "lblSandwichOptions3";
+            this.lblSandwichOptions3.Size = new System.Drawing.Size(131, 13);
+            this.lblSandwichOptions3.TabIndex = 16;
+            this.lblSandwichOptions3.Text = "bacon, lettuce and tomato";
+            // 
+            // lblSandwichOptions4
+            // 
+            this.lblSandwichOptions4.AutoSize = true;
+            this.lblSandwichOptions4.Location = new System.Drawing.Point(35, 344);
+            this.lblSandwichOptions4.Name = "lblSandwichOptions4";
+            this.lblSandwichOptions4.Size = new System.Drawing.Size(237, 13);
+            this.lblSandwichOptions4.TabIndex = 17;
+            this.lblSandwichOptions4.Text = "egg, tomato, alfa sprouts, cucumber and mustard";
+            // 
+            // lblSandwichOptions5
+            // 
+            this.lblSandwichOptions5.AutoSize = true;
+            this.lblSandwichOptions5.Location = new System.Drawing.Point(35, 357);
+            this.lblSandwichOptions5.Name = "lblSandwichOptions5";
+            this.lblSandwichOptions5.Size = new System.Drawing.Size(210, 13);
+            this.lblSandwichOptions5.TabIndex = 18;
+            this.lblSandwichOptions5.Text = "chicken, creamy cheese, salad and apricot";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 400);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(280, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "tomato, onion, creamy cheese, avocado and tomato relish";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.ClientSize = new System.Drawing.Size(467, 465);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblSandwichOptions5);
+            this.Controls.Add(this.lblSandwichOptions4);
+            this.Controls.Add(this.lblSandwichOptions3);
+            this.Controls.Add(this.lblSandwichOptions2);
+            this.Controls.Add(this.lblBagels);
+            this.Controls.Add(this.lblBagelOptions);
+            this.Controls.Add(this.lblSandwichs);
+            this.Controls.Add(this.lblSandwichOptions);
+            this.Controls.Add(this.lblCheesePuffs);
+            this.Controls.Add(this.lblBrownie);
+            this.Controls.Add(this.lblMuffinSweet);
+            this.Controls.Add(this.lblCinnamonSwirl);
+            this.Controls.Add(this.lblSavouryPinwheel);
+            this.Controls.Add(this.lblMuffinSavoury);
+            this.Controls.Add(this.lblCheeseRolls);
+            this.Controls.Add(this.btnLunch);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.picBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Bayfield Highschool Canteen Menu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnLunch;
+        private System.Windows.Forms.Label lblCheeseRolls;
+        private System.Windows.Forms.Label lblMuffinSavoury;
+        private System.Windows.Forms.Label lblSavouryPinwheel;
+        private System.Windows.Forms.Label lblCinnamonSwirl;
+        private System.Windows.Forms.Label lblMuffinSweet;
+        private System.Windows.Forms.Label lblBrownie;
+        private System.Windows.Forms.Label lblCheesePuffs;
+        private System.Windows.Forms.Label lblSandwichOptions;
+        private System.Windows.Forms.Label lblSandwichs;
+        private System.Windows.Forms.Label lblBagelOptions;
+        private System.Windows.Forms.Label lblBagels;
+        private System.Windows.Forms.Label lblSandwichOptions2;
+        private System.Windows.Forms.Label lblSandwichOptions3;
+        private System.Windows.Forms.Label lblSandwichOptions4;
+        private System.Windows.Forms.Label lblSandwichOptions5;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
